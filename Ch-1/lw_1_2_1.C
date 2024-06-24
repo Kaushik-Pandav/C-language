21263878f8eb6a4a220b8ee1ffb72b978cb09e7e
Kaushik-Pandav <kaushikpandav106@gmail.com>
2024-06-24 16:53:42 +0530

    Ch 1: Introduction to C-language

---
+++ Ch-1/lw_1_2_1.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+	printf("Name\t:Kaushik Dilipbhai Pandav\n");
+	printf("Age\t:24\n");
+	printf("School\t:I.C.Gandhi High School");
+	getch();
+}
