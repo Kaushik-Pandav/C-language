519a8504e2a0d86237ec56537044c5cfce36fd61
Kaushik-Pandav <kaushikpandav106@gmail.com>
2024-07-17 17:31:41 +0530

    self

--- Ch-11/sw_7.c
+++ Ch-11/sw_7.c
@@ -4,13 +4,15 @@ int main()
     int a=0,b=1,c=0;
     for(int i=1;i<=4;i++)
     {
+        printf("%d ",a);
+        printf("%d ",b);
         for(int j=3;j<=i+2;j++)
         {
-            printf("%d ",c);
             c=a+b;
+            printf("%d ",c);
             b=a;
             c=b;
-            
+            a=c;
         }
         printf("\n");
     }
